b801643493c1b1a9e2d8d58578e85f0e53ab204a
Andrey Puzanov <puzanov.ee@gmail.com>
2024-11-26 19:35:01 +0300

    Added Quick Sort

---
+++ Sorting/quick_sort.py
@@ -0,0 +1,21 @@
+def quick_sort(seq):
+    if len(seq) <= 1:
+        return seq
+
+    pivot = seq.pop()
+
+    items_lower, items_greater = [], []
+
+    for item in seq:
+        if item < pivot:
+            items_lower.append(item)
+        else:
+            items_greater.append(item)
+
+    return quick_sort(items_lower) + [pivot] + quick_sort(items_greater)
+
+
+# Testing
+x = [1, 6, 4, 2, 7, 9, 0, 99]
+
+print(quick_sort(x))
