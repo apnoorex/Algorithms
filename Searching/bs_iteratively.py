a067e95df0746fe6136ec42202b74e52d8fffda5
Andrey Puzanov <puzanov.ee@gmail.com>
2024-11-29 09:50:51 +0300

    Add Binary Search algorithm

---
+++ Searching/bs_iteratively.py
@@ -0,0 +1,23 @@
+def binary_search(sequence, item):
+    """ Binary search algorithm.The sequence has to be sorted first. """
+    start_index = 0
+    end_index = len(sequence) - 1
+
+    while start_index <= end_index:
+        midpoint = start_index + (end_index - start_index) // 2
+        midpoint_value = sequence[midpoint]
+        if midpoint_value == item:
+            return midpoint
+        
+        elif midpoint_value > item:
+            end_index = midpoint - 1
+
+        else:
+            start_index = midpoint + 1
+
+    return None
+
+
+# Testing
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(binary_search(a, 9))
---
+++ Searching/bs_recursively.py
@@ -0,0 +1,21 @@
+def binary_search(sequence, start_index, end_index, item):
+    """ Binary search algorithm.The sequence has to be sorted first. """
+    if start_index <= end_index:
+        midpoint = (start_index + end_index) // 2
+
+        if sequence[midpoint] == item:
+            return midpoint
+        
+        elif item < sequence[midpoint]:
+            return binary_search(sequence, start_index, midpoint-1, item)
+        
+        else:
+            return binary_search(sequence, midpoint+1, end_index, item)
+        
+    else:
+        return None
+    
+
+# Testing
+x = [1, 4, 6, 8, 9, 45, 67, 88, 99]
+print(binary_search(x, 0, len(x), 67))
