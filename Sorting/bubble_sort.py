5721b07ed3c4abb07c0d19ea87639367ce5b5483
Andrey Puzanov <puzanov.ee@gmail.com>
2024-11-29 09:56:57 +0300

    Add Bubble Sort

---
+++ Sorting/bubble_sort.py
@@ -0,0 +1,16 @@
+def bubble_sort(seq):
+    sorted = False
+
+    while not sorted:
+        sorted = True
+
+        for idx in range(len(seq) - 1):
+            if seq[idx] > seq[idx+1]:
+                seq[idx], seq[idx+1] = seq[idx+1], seq[idx]
+                sorted = False
+    return seq
+
+
+# Testing
+x = [1, 4, 3, 2, 1, 67, 8, 0, 5]
+print(bubble_sort(x))
