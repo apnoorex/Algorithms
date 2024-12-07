9365fe9061409e9bd77217eeaad7fe61f96c9903
Andrey Puzanov <puzanov.ee@gmail.com>
2024-12-07 13:24:23 +0300

    Cleaning

--- Sorting/counting_sort.py
+++ Sorting/counting_sort.py
@@ -1,24 +1,24 @@
-def counting_sort(sequence):
+def counting_sort(seq):
     """
     Counting sort for positive integers.
     Complexity: O(n + k), where:
     n - number of elements
     k - range of input sequence
     """
-    max_value = max(sequence)
+    max_value = max(seq)
     count_array = [0] * (max_value + 1)
-    output_array = [0] * len(sequence)
+    output_array = [0] * len(seq)
 
-    for num in sequence:
+    for num in seq:
         count_array[num] += 1
 
     # Add previous counts
     for idx in range(1, max_value + 1):
         count_array[idx] += count_array[idx - 1]
 
-    for idx in range(len(sequence) - 1, -1, -1):
-        output_array[count_array[sequence[idx]] - 1] = sequence[idx]
-        count_array[sequence[idx]] -= 1
+    for idx in range(len(seq) - 1, -1, -1):
+        output_array[count_array[seq[idx]] - 1] = seq[idx]
+        count_array[seq[idx]] -= 1
 
     return output_array
 
